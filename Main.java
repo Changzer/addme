7c95fbccb0be9e53e487b8e61a3845cdde125d99
Changzer <85975702+Changzer@users.noreply.github.com>
2021-09-29 14:51:12 -0400

    mais um teste de system out

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main {
 
     public static void main(String[]args){
-        System.out.println("hello world 2");
+
+        System.out.println("hello world 3");
+        System.out.println("ainda funciona?");
     }
+
 }
